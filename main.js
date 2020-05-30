2e324a16c65774daac9c88629d11075c13e1c305
janet-aharonyan <60620568+janet-aharonyan@users.noreply.github.com>
2020-05-31 01:17:31 +0400

    Create main.js

---
+++ main.js
@@ -0,0 +1,8 @@
+// HackerRank Day1: Let and Const
+
+function main(r) {
+    let area = Math.PI * Math.pow(r, 2);
+    let perimeter = 2 * Math.PI * r;
+    console.log(area);
+    console.log(perimeter);
+  }
