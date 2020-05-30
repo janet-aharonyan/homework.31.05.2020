f23b1d0b37c95548d63692cde6eeb80c539e4d51
janet-aharonyan <60620568+janet-aharonyan@users.noreply.github.com>
2020-05-31 01:16:08 +0400

    Create performOperation.js

---
+++ performOperation.js
@@ -0,0 +1,10 @@
+// HackerRank Day0: Data Types
+
+function performOperation(secondInteger, secondDecimal, secondString) {
+    const firstInteger = 4;
+    const firstDecimal = 4.0;
+    const firstString = 'HackerRank ';
+    console.log(Number(firstInteger) + Number(secondInteger));
+    console.log(firstDecimal + Number(secondDecimal));
+    console.log(firstString + secondString);
+  }
